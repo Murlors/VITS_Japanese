097c4df1977ee001eb4e6e1b22beb77f290a6442
1595258509 <1595258509@qq.com>
2022-08-10 22:24:42 +0800

    gloo in Windows

--- train_ms.py
+++ train_ms.py
@@ -63,7 +63,7 @@ def run(rank, n_gpus, hps):
     utils.check_git_hash(hps.model_dir)
     writer = SummaryWriter(log_dir=hps.model_dir)
     writer_eval = SummaryWriter(log_dir=os.path.join(hps.model_dir, "eval"))
-
+  # gloo in Windows
   dist.init_process_group(backend='nccl', init_method='env://', world_size=n_gpus, rank=rank)
   torch.manual_seed(hps.train.seed)
   torch.cuda.set_device(rank)
