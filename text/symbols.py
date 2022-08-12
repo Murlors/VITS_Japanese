0ee192124331a10f7edb3dfd3415c90ee80cc2c8
1595258509 <1595258509@qq.com>
2022-08-12 08:46:40 +0800

    symbols update.

--- text/symbols.py
+++ text/symbols.py
@@ -10,17 +10,17 @@ _punctuation = ',.!?-'
 _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧ↓↑ '
 '''
 
-'''# japanese_cleaners2
+# japanese_cleaners2
 _pad        = '_'
 _punctuation = ',.!?-'
 _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧʦ↓↑ '
-'''
 
-# korean_cleaners
+
+'''# korean_cleaners
 _pad        = '_'
 _punctuation = ',.!?…~'
 _letters = 'ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉㅏㅓㅗㅜㅡㅣㅐㅔ '
-
+'''
 
 # Export all symbols:
 symbols = [_pad] + list(_punctuation) + list(_letters)
